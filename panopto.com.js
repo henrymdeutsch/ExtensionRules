1e737620cbf66c676b3f45a7cfa0eb2cdb0474e5
Henry Deutsch <henrymdeutsch@gmail.com>
2022-06-26 10:29:04 -0400

    added incremental button presses j & k, solved bugs, cleaned up code

--- panopto.com.js
+++ panopto.com.js
@@ -1,5 +1,31 @@
+
 // video el
 el = document.getElementById("primaryVideo");
+
+// solves occasional rounding errors, where 11 - 1 is 9.99999993 for ex.
+function less_than_10 (num) {
+  if (num > 10) return false;
+  else if (Math.abs(num - 10) < 0.001) return false; // is close to 10
+  return true;
+}
+
+function less_than_or_equal_10 (num) {
+  if (num > 10) return false;
+  else if (Math.abs(num - 10) < 0.001) return true; // is close to 10
+  return true;
+}
+
+function greater_than_or_equal_10 (num) {
+  if (num > 10) return true;
+  else if (Math.abs(num - 10) < 0.001) return true; // is close to 10
+  return false;
+}
+
+function display_speed (num) {
+  return `${greater_than_or_equal_10(num) ? 
+    num.toFixed(0) : 
+    num.toFixed(1)}x`;
+}
 
 document.onkeydown = function (e) {
   e = e || window.event;
@@ -13,7 +39,7 @@ document.onkeydown = function (e) {
   div.style.right = '10px';
   div.style.backgroundColor = '#2b2b2b';
   div.style.width = '50px';
-  div.style.height = '20px';
+  div.style.padding = '2px';
   div.style.borderRadius = '10px';
   div.style.zIndex = '999999';
   div.style.textAlign = 'center';
@@ -25,14 +51,19 @@ document.onkeydown = function (e) {
   div.style.transitionDelay = '0s';
   div.innerHTML = '[Error]';
   
-  // letters in between numbers -or- shift+num -> +0.5. 
-  // '0': 10x, '-': 20x, '=': 30x.
-  // w or d: +0.5, s or a: -0.5
+  // numbers and letters except i: ± 0.5 
+  // '~': 0.5x, '0': 10x, '-': 20x, '=': 30x.
+  // j- k+
+  // (shift+num : ± 0.5)
+  let old_speed = el.playbackRate;
   let new_speed;
+  let small = 0.001;
+  let useful_key = true;
   switch (e.keyCode) {
-    // w-s-a-d: up, down, left, right
-    case 87: case 68: new_speed = el.playbackRate + 0.5; break; // up
-    case 83: case 65: new_speed = el.playbackRate - 0.5; break; // down
+    // j- k+
+    case 74: new_speed = old_speed - (less_than_or_equal_10(old_speed) ? 0.1 : 1); break; // j
+    case 75: new_speed = old_speed + (less_than_10(old_speed) ? 0.1 : 1); break; // k
+    
     // numbers
     case 192: new_speed = 0.5; break; // ~
     case 49: new_speed = 1; break; // 1
@@ -47,7 +78,7 @@ document.onkeydown = function (e) {
     case 48: new_speed = 10; break; // 0
     case 189: new_speed = 20; break; // -
     case 187: new_speed = 30; break; // =
-    // shift -> +0.5 speed
+    // letters between nums -or- shift -> +0.5 speed    // reserve i for inspect element command
     case 33: case 81: new_speed = 1.5; break; // ! or q
     case 64: case 87: new_speed = 2.5; break; // @ or w
     case 35: case 69: new_speed = 3.5; break; // # or e
@@ -55,18 +86,21 @@ document.onkeydown = function (e) {
     case 37: case 84: new_speed = 5.5; break; // % or t
     case 94: case 89: new_speed = 6.5; break; // ^ or y
     case 38: case 85: new_speed = 7.5; break; // & or u
-    case 42: case 73: new_speed = 8.5; break; // * or i
+    case 42: /*case 73:*/ new_speed = 8.5; break; // *
     case 40: case 79: new_speed = 9.5; break; // ( or o
     case 41: case 80: new_speed = 10.5; break; // ) or p
+    default: useful_key = false;
   }
-  console.log(`\t\t--- NEW PLAYBACK RATE: ${new_speed} ---`); 
-  el.playbackRate = new_speed;
-  div.innerHTML = `{new_speed}`; // can this be an int? idk
+  if (useful_key) {
+    console.log(`\t\t--- NEW PLAYBACK RATE: ${display_speed(new_speed)} ---`); 
+    el.playbackRate = new_speed;
+    div.innerHTML = display_speed(new_speed);
 
-  // show keypress tracker, then fade
-  document.body.appendChild(div);
-  setTimeout(function() {
-    div.style.opacity = 0;
-  }, 1000);
+    // show keypress tracker, then fade
+    document.body.appendChild(div);
+    setTimeout(function() {
+      div.style.opacity = 0;
+    }, 1000);
+  } else console.log(`\t\t--- KEYPRESS IGNORED ---`);
 
 };
